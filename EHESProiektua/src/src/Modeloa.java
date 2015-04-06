2e29dbb1375935e8f026768c249a75488d74a764
ieltzu <iirazu007@ikasle.ehu.es>
2015-04-06 23:05:33 +0200

    modeloa hasita

--- EHESProiektua/src/src/Modeloa.java
+++ EHESProiektua/src/src/Modeloa.java
@@ -1,13 +1,28 @@
 package src;
 
+import weka.core.Instances;
+
 public class Modeloa {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//trainaurre eta devaurre entrenamendu multzoak jasoko dira. Lehenik beti jarriko da train multzoa eta gero dev multzoa
+		
+		Instances trainaurre = Irakurtzailea.getIrakurtzailea().instantziakIrakurri(args[0]);
+		Instances devaurre = Irakurtzailea.getIrakurtzailea().instantziakIrakurri(args[1]);
+		
+		// Baseline (Naive Bayes)
+		
+		
+		
+		Idazlea.getIdazlea().fitxategiakEgin(datuak);
+		Idazlea.getIdazlea().modeloaIdatzi(mod, cls);
+		
+		// Multilayer Perceptron
+		
+		Idazlea.getIdazlea().fitxategiakEgin(datuak);
+		Idazlea.getIdazlea().modeloaIdatzi(mod, cls);
 	}
-
+	
+	
 }
--- EHESProiektua/src/src/Sailkatzailea.java
+++ EHESProiektua/src/src/Sailkatzailea.java
@@ -2,9 +2,7 @@ package src;
 
 public class Sailkatzailea {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
